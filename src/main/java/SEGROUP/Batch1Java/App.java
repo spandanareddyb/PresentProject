b34b984811fd5ad91a3964282b5fdabc7aa49317
spandana/ <spandanareddybanka@gmail.com>
2024-11-19 14:46:25 +0530

    demo for team

---
+++ src/main/java/SEGROUP/Batch1Java/App.java
@@ -0,0 +1,13 @@
+package SEGROUP.Batch1Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SEGROUP/Batch1Java/AppTest.java
@@ -0,0 +1,20 @@
+package SEGROUP.Batch1Java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
